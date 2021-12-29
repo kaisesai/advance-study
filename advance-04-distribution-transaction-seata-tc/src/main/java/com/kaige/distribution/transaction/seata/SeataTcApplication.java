19b0f82bffcf9ecf200771e03e5192c54122c57d
liukai <liukai@yzyw-inc.com>
2021-12-30 01:00:58 +0800

    feat: 分布式事务
    1. seata 服务端包

---
+++ advance-04-distribution-transaction-seata-tc/src/main/java/com/kaige/distribution/transaction/seata/SeataTcApplication.java
@@ -0,0 +1,13 @@
+package com.kaige.distribution.transaction.seata;
+
+import io.seata.server.Server;
+
+/**
+ * Seata 服务的启动类
+ *
+ * <p>Server 类是使用 netty 启动服务的，同时执行命令行对应的参数
+ *
+ * @author kaige
+ * @date 2021年12月30日
+ */
+public class SeataTcApplication extends Server {}
